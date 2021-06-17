37d72a720c2728d6a9866fc2b785c7d96d66c79e
EC2 Default User <ec2-user@ip-172-31-0-90.ap-northeast-1.compute.internal>
2021-06-17 08:57:36 +0000

    電卓

---
+++ main.js
@@ -0,0 +1,33 @@
+let currentTotal = 0;
+let input = "";
+let calcOperator = "+";
+let status = 1;
+
+function btnNumber(myData){
+    status = 0;
+    input += myData;
+    document.subjectCalculator.output.value = input;
+}
+
+function btnOperation(myData){
+    if(status == 0){
+        status = 1;
+        let calcFormula = currentTotal + calcOperator + input;
+        currentTotal = (new Function("return " + calcFormula))();
+        input= "";
+        document.subjectCalculator.output.value = currentTotal;
+    }
+    if(myData == "="){
+        currentTotal = 0;
+        calcOperator = "+";
+    } else{
+        calcOperator = myData;
+    }
+}
+
+function allClear(){
+    currentTotal = 0;
+    calcOperator = "+";
+    input = "";
+    document.subjectCalculator.output.value = currentTotal;
+}
